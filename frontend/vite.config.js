50e33f2b17406cf6e80c412f3798380331fe5f4b
Siri Chunchu <sirichunchu123@gmail.com>
2026-06-09 16:05:59 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': { target: 'https://algo-forge-dvq6.onrender.com', changeOrigin: true },
+      '/api': { target: 'https://algo-forge-dvq6.onrender.com/api', changeOrigin: true },
     },
   },
 });
